825dae7fff6818d262a4d6b7509e671d122111ac
Halissa <halissa.franco@outlook.com>
2024-07-14 10:45:28 -0300

    3 tests passaram

--- desafios/tdd-project/tests/usecases/test_product.py
+++ desafios/tdd-project/tests/usecases/test_product.py
@@ -6,3 +6,4 @@ async def test_usecases_create_should_return_success(product_in):
     result = await product_usecase.create(body=product_in)
 
     assert isinstance(result, ProductOut)
+    assert result.name == "Iphone 14 pro Max"
