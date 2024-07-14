ad3524f689df4ec1ed699c0e8ea517e60911b865
Halissa <halissa.franco@outlook.com>
2024-07-14 10:43:54 -0300

    3 testes passaram

--- desafios/tdd-project/tests/usecases/test_product.py
+++ desafios/tdd-project/tests/usecases/test_product.py
@@ -1,8 +1,8 @@
+from store.schemas.product import ProductOut
 from store.usecases.product import product_usecase
 
 
-async def test_usecases_should_return_sucess(product_in):
+async def test_usecases_create_should_return_success(product_in):
     result = await product_usecase.create(body=product_in)
 
-    # assert isinstance(result, ProductOut)
-    assert result is None
+    assert isinstance(result, ProductOut)
